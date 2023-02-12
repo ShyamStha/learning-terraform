a08b55c067c1aa12ec307085a4f147ae4cee2a4f
Shyam Shrestha <shyamstha055@gmail.com>
2023-02-12 19:45:19 +0545

    Use existing VPC

--- main.tf
+++ main.tf
@@ -14,9 +14,6 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-data "aws_vpc" "default" {
-  default = true
-}
 
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
@@ -33,7 +30,7 @@ resource "aws_security_group" "blog" {
   name        = "blog"
   description = "Allow http and https in. Allow everything out"
 
-  vpc_id = data.aws_vpc.default.id
+  vpc_id = vpc-09e21617c56ae11ce
 }
 
 resource "aws_security_group_rule" "blog_http_in" {
